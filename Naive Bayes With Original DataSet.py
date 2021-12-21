c97b497a7242ecb4a694342e482bae3068778ca8
Nishant Malik <60120827+nishantmalik20@users.noreply.github.com>
2021-12-20 23:04:29 -0500

    NB with Original Dataset

---
+++ Naive Bayes With Original DataSet.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import re
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import warnings
+import json
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+jsonList = []
+SRC=[]
+TGT=[]
+count=0
+print("Started Reading JSON file which contains multiple JSON document")
+with open(r'C:\Users\Smrijay\OneDrive\Desktop\github-typo-corpus-master\github-typo-corpus.v1.0.0.jsonl', 'r', encoding='utf8') as jsonFile:
+    for jsonObj in jsonFile:
+        dictionary = json.loads(jsonObj)
+        jsonList.append(dictionary)
+count=0
+print("Printing each JSON Decoded Object")
+for obj in jsonList:
+    if(count<5000):
+        key='is_typo'
+    #key2='edits'
+    #for keys2 in obj:   
+        if key in obj['edits'][0]:
+            SRC.append(obj['edits'][0]['src']['text'])
+        #key='is_typo'
+        #if key in obj:
+        #print(obj['edits'][0]['is_typo'])
+        #if key in obj['edits'][0]:
+            if(obj['edits'][0]['is_typo']==True):
+        
+                TGT.append('typo')
+            else:
+                TGT.append('non typo')
+        #else:
+            #TGT.append('non typo')
+count=count+1
+
+#vectorization
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=3000)
+X = cv.fit_transform(SRC).toarray()
+y=TGT
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state = 0)
+#using Naive bayes classifier
+print('Naive bayers')
+from sklearn.naive_bayes import MultinomialNB
+baseline_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred=baseline_model.predict(X_test)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+
+
+# In[ ]:
+
+
+
+
